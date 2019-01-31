d60eb3d6a957316ed3058b4c58f2ad9a5e05906a
SLS\mpalavelli <mahan.palavelli.consultant@nielsen.com>
2019-01-31 16:53:50 +0530

    Third

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,10 +4,13 @@ export env=$1
 export version=$2
 JAR_NAME=/home/travis/build/mahankaleswararao/ScalaExample/target/ScalaExample-${version}.jar
 echo "${JAR_NAME}"
+pwd
+
 CLASS_NAME="com.kali.FirstProgram"
+echo "$SCALA_HOME"
 
 if [ ${env} = "dev" ] || [ ${env} = "staging" ] || [ ${env} = "prod" ]; then 
-    java -cp ${JAR_NAME} ${CLASS_NAME}
+    scala -cp ${JAR_NAME} ${CLASS_NAME}
 else 
     echo "Invalid Environment"
 exit 1
--- src/main/scala/com/kali/FirstProgram.scala
+++ src/main/scala/com/kali/FirstProgram.scala
@@ -4,5 +4,6 @@ object FirstProgram {
   def main(args: Array[String]): Unit = {
     println("Hello World")
     println("Hello World")
+    
   }
 }
