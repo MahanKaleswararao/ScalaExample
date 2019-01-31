93fd308d78f99aaf3952e76074550f40f6a1d504
MahanKaleswararao <47206950+MahanKaleswararao@users.noreply.github.com>
2019-01-31 16:36:25 +0530

    Update deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,10 +3,11 @@
 export env=$1
 export version=$2
 JAR_NAME=/home/travis/build/mahankaleswararao/ScalaExample/target/ScalaExample-${version}.jar
+echo "${JAR_NAME}"
 CLASS_NAME="com.kali.FirstProgram"
 
 if [ ${env} = "dev" ] || [ ${env} = "staging" ] || [ ${env} = "prod" ]; then 
-    scala -cp ${JAR_NAME} ${CLASS_NAME}
+    scala -classpath ${JAR_NAME} ${CLASS_NAME}
 else 
     echo "Invalid Environment"
 exit 1
