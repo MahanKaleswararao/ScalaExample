c282a10a500a57b33c9154c1ff0090db154193e8
SLS\mpalavelli <mahan.palavelli.consultant@nielsen.com>
2019-01-31 16:06:22 +0530

    Second

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,12 +2,12 @@
 
 export env=$1
 export version=$2
-JAR_NAME=/home/travis/build/mahankaleswararao/HelloWorldJava/target/HelloWorldJava-${version}.jar
+JAR_NAME=/home/travis/build/mahankaleswararao/ScalaExample/target/ScalaExample-${version}.jar
 CLASS_NAME="com.kali.FirstProgram"
 
 if [ ${env} = "dev" ] || [ ${env} = "staging" ] || [ ${env} = "prod" ]; then 
-java -cp ${JAR_NAME} ${CLASS_NAME}
+    scala -cp ${JAR_NAME} ${CLASS_NAME}
 else 
-echo "Invalid Environment"
+    echo "Invalid Environment"
 exit 1
 fi
